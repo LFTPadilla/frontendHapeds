ae748e0c74c87769358e52426684854a436fce36
LUIS FELIPE TEJADA PADILLA <lftejadap@uqvirtual.edu.co>
2020-10-26 19:45:22 -0500

    First commit frontend

---
+++ src/app/business/auth/user-profile-business.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserProfileBusinessService {
+
+  baseurl = '';
+  httpHeaders = new HttpHeaders({'Content-Type':'application/json','Access-Control-Allow-Origin':'*'});
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getUser(id:number): Observable<any> {
+    return this.httpClient.get( this.baseurl+ '/users/'+id+'/?format=json',
+    {headers: this.httpHeaders});
+  }
+
+
+}
---
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { UserProfileBusinessService } from 'src/app/business/auth/user-profile-business.service';
+import { User } from 'src/app/model/user';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss'],
+  providers:[UserProfileBusinessService]
+})
+export class UserProfileComponent implements OnInit {
+
+  myUser: User;
+
+  constructor(private userBusiness: UserProfileBusinessService) {
+    this.getProjects();
+  }
+
+  getProjects() {
+    this.userBusiness.getUser(1).subscribe(data =>{
+      this.myUser = data[0];
+      console.log(this.myUser);
+    }, error =>{
+      console.log(error);
+    })
+  }
+
+  ngOnInit(){
+
+  }
+}
