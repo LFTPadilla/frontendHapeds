d44ec505efcc198a785316c301666b777a16ff46
LUIS FELIPE TEJADA PADILLA <lftejadap@uqvirtual.edu.co>
2020-10-30 17:59:11 -0500

    Sync the old proyect

---
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { UserProfileBusinessService } from 'src/app/business/auth/user-profile-business.service';
+import { User } from 'src/app/model/user';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss'],
+  providers:[UserProfileBusinessService]
+})
+export class UserProfileComponent implements OnInit {
+
+  myUser: User = new User();
+
+  constructor(private userBusiness: UserProfileBusinessService) {
+    this.getUser();
+  }
+
+  getUser() {
+    this.userBusiness.getUser(1).subscribe(data =>{
+      console.log("Datos " + data );
+      this.myUser = data;
+      console.log(this.myUser);
+    }, error =>{
+      console.log(error);
+    })
+  }
+
+  ngOnInit(){
+
+  }
+}
