eb82a445dbc6e4d3ece3356c27261ff4a55f9eaf
LUIS FELIPE TEJADA PADILLA <lftejadap@uqvirtual.edu.co>
2020-10-30 19:09:29 -0500

    Delete everything: second time

--- src/app/business/services/igateway.ts
+++
@@ -1,6 +0,0 @@
-export interface IGateway<T> {
-    GetSingle(filters:any):Promise<T>;
-    GetCollection(filters:any):Promise<T[]>;
-    Save(object:T):void;
-    SaveCollection(object:T[]):void;
-}
