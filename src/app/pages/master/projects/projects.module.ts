f4bb8b7fe50cbc30cbb3b672148eaebcdd5b6f1c
AlejandraCaicedo <caicedomaria905@gmail.com>
2020-11-07 13:01:22 -0500

    creacion nuevo componente

--- src/app/pages/master/projects/projects.module.ts
+++ src/app/pages/master/projects/projects.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects.component';
+import { TasksModule } from '../tasks/tasks.module';
 
 
 
 @NgModule({
   declarations: [ProjectsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TasksModule
   ]
 })
 export class ProjectsModule { }
