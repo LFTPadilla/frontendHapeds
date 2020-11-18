92c0c2a0fed74232ae02e27378f8fc44634d2990
LUIS FELIPE TEJADA PADILLA <lftejadap@uqvirtual.edu.co>
2020-11-18 11:00:51 -0500

    Quitar edit

--- src/app/pages/master/planner-scrum/planner-scrum.module.ts
+++ src/app/pages/master/planner-scrum/planner-scrum.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PlannerScrumComponent } from './planner-scrum.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IterationEditModule } from '../iteration-edit/iteration-edit.module';
+
 import { IterationEditComponent } from '../iteration-edit/iteration-edit.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ProjectEditComponent } from '../project-edit/project-edit.component';
