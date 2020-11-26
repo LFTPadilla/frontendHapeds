422e6d6947b22104e3cb3346c729c3588c7afa2b
Juan Manuel Roa Mejia <juanmanuelrm2009@gmail.com>
2020-11-26 14:10:01 -0500

    List Requirements Full

--- src/app/pages/master/requirement-edit/requirement-edit.component.ts
+++ src/app/pages/master/requirement-edit/requirement-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectBusinessService } from 'src/app/business/master/project-business.service';
 import { RequirementsBusinessService } from 'src/app/business/master/requirements-business.service';
+import { Project } from 'src/app/model/project';
 import { Requirement } from 'src/app/model/Requirement';
 
 @Component({
@@ -11,21 +13,34 @@ import { Requirement } from 'src/app/model/Requirement';
 export class RequirementEditComponent implements OnInit {
 
   RequirementOpened=new Requirement();
+  Projects: Project[] = [];
+  ProjectSelected:  Project = null;
 
   @Output() OnSaveEvent = new EventEmitter<boolean>();
 
   @ViewChild( 'ModalEdit',{static: false} ) modalEdit;
 
-  constructor(private modalService: NgbModal,private requirementBussines: RequirementsBusinessService  ) { }
+  constructor(private modalService: NgbModal,private requirementBussines: RequirementsBusinessService,
+    private projectBussines: ProjectBusinessService ) { 
+      this.GetProjects();
+    }
 
   LaunchModal(req: Requirement) {
     Object.assign(this.RequirementOpened,req);
+    
+
+
+    this.modalService.open(this.modalEdit, {ariaLabelledBy: 'modal-basic-title'});
+  }
+
+  LaunchModalCreate() {
 
     this.modalService.open(this.modalEdit, {ariaLabelledBy: 'modal-basic-title'});
   }
 
   SaveRequirement(){
 
+    this.RequirementOpened.ProjectId = this.ProjectSelected.ProjectId
     this.requirementBussines.SaveRequirement(this.RequirementOpened).then(x=> {
       this.modalService.dismissAll('Save');
       this.OnSaveEvent.emit();
@@ -34,6 +49,16 @@ export class RequirementEditComponent implements OnInit {
       //console.log("error "+x)
     });
 
+  }
+
+  GetProjects() {
+    this.projectBussines.GetProjects()
+      .then(x => {
+        this.Projects = x;
+        //console.log("Se cargaron correctamente los proyectos" + x);
+      }).catch(x => {
+        //console.log("error en los proyectos" + x)
+      })
   }
 
   ngOnInit(): void {
