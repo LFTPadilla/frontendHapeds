251283673103bd2d610c20c14cb1dcb8be36e030
LUIS FELIPE TEJADA PADILLA <lftejadap@uqvirtual.edu.co>
2020-11-30 14:42:32 -0500

    register

--- src/app/pages/authentication/register/register.component.ts
+++ src/app/pages/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthorizationBusinessService } from 'src/app/business/security/authorization-business.service';
@@ -15,6 +16,7 @@ export class RegisterComponent implements OnInit {
   isSubmit:boolean;
   politicsCheck =false;
   onAction = false;
+  @ViewChild('registerForm', {static: false}) RegisterForm:NgForm;
 
   constructor(private business : AuthorizationBusinessService,
     private router: Router) {
@@ -23,11 +25,19 @@ export class RegisterComponent implements OnInit {
 
 
   RegisterUser(){
+    this.RegisterForm.form.markAllAsTouched();
+    if(this.RegisterForm.form.invalid){
+      Swal.fire({
+        title: 'Advertencia',
+        text: 'Formulario inválido',
+        icon: 'warning',
+        confirmButtonText: 'Cerrar'
+      });
+      return;
+    }
+
     this.onAction = true;
-
     this.business.RegisterUser(this.formDataRegister).then(x => {
-
-
         Swal.fire({
           title: "Éxito",
           text: "Se creó el usuario correctamente. Por favor inicie session.",
@@ -63,16 +73,16 @@ export class RegisterComponent implements OnInit {
        ' identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este'+
        ' documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada'+
        ' por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que'+
-       ' está de acuerdo con dichos cambios.<br><br>'+ 
+       ' está de acuerdo con dichos cambios.<br><br>'+
 
        '<b>Información que es recogida</b><br>'+
-      
+
       'Nuestro sitio web podrá recoger información personal por ejemplo: Nombre,  información de'+
       ' contacto como  su dirección de correo electrónica e información demográfica. Así mismo cuando'+
       ' sea necesario podrá ser requerida información específica para procesar algún pedido o realizar'+
       ' una entrega o facturación.<br><br>'+
       '<b>Uso de la información recogida</b><br>'+
-      
+
       'Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible,'+
       ' particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar'+
       ' nuestros productos y servicios.  Es posible que sean enviados correos electrónicos'+
@@ -80,19 +90,19 @@ export class RegisterComponent implements OnInit {
       ' información publicitaria que consideremos relevante para usted o que pueda brindarle algún'+
       ' beneficio, estos correos electrónicos serán enviados a la dirección que usted proporcione y'+
       ' podrán ser cancelados en cualquier momento.<br><br>'+
-      
+
       'Zoftinium está altamente comprometido para cumplir con el compromiso de mantener su información'+
       ' segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos que'+
       ' no exista ningún acceso no autorizado.<br><br>'+
-      
+
       '<b>Cookies</b><br>'+
-      
+
       'Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para'+
       ' almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve entonces para'+
       ' tener información respecto al tráfico web, y también facilita las futuras visitas a una web'+
       ' recurrente. Otra función que tienen las cookies es que con ellas las web pueden reconocerte'+
       ' individualmente y por tanto brindarte el mejor servicio personalizado de su web.<br><br>'+
-      
+
       'Nuestro sitio web emplea las cookies para poder identificar las páginas que son visitadas y su'+
       ' frecuencia. Esta información es empleada únicamente para análisis estadístico y después la'+
       ' información se elimina de forma permanente. Usted puede eliminar las cookies en cualquier'+
@@ -103,27 +113,27 @@ export class RegisterComponent implements OnInit {
       ' tener un mejor servicio web. También usted puede cambiar la configuración de su ordenador para'+
       ' declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros'+
       ' servicios.<br><br>'+
-      
+
       '<b>Enlaces a Terceros</b><br>'+
-      
+
       'Este sitio web pudiera contener en laces a otros sitios que pudieran ser de su interés. Una vez'+
       ' que usted de clic en estos enlaces y abandone nuestra página, ya no tenemos control sobre al'+
       ' sitio al que es redirigido y por lo tanto no somos responsables de los términos o privacidad ni'+
       ' de la protección de sus datos en esos otros sitios terceros. Dichos sitios están sujetos a sus'+
       ' propias políticas de privacidad por lo cual es recomendable que los consulte para confirmar que'+
       ' usted está de acuerdo con estas.<br><br>'+
-      
+
       '<b>Control de su información personal</b><br>'+
-      
+
       'En cualquier momento usted puede restringir la recopilación o el uso de la información personal'+
       ' que es proporcionada a nuestro sitio web.  Cada vez que se le solicite rellenar un formulario,'+
       ' como el de alta de usuario, puede marcar o desmarcar la opción de recibir información por'+
       ' correo electrónico.  En caso de que haya marcado la opción de recibir nuestro boletín o'+
       ' publicidad usted puede cancelarla en cualquier momento.<br><br>'+
-      
+
       'Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin'+
       ' su consentimiento, salvo que sea requerido por un juez con un orden judicial.<br><br>'+
-      
+
       'Zoftinium Se reserva el derecho de cambiar los términos de la presente Política de Privacidad'+
       ' en cualquier momento.</p>',
       icon: 'info',
